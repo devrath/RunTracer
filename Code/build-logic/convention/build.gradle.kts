54084be638dd3bb898ba5d5a0feb69b635787a0e
Devrath <devrath.dev595@gmail.com>
2024-06-12 23:59:44 +0530

    a

--- Code/build-logic/convention/build.gradle.kts
+++ Code/build-logic/convention/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
+    `kotlin-dsl`
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+group = "com.istudio.runtracer.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
 }
