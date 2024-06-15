eec905e92de19f48204db13793593b8b1d706315
Devrath <devrath.dev595@gmail.com>
2024-06-15 12:15:49 +0530

    added build type

--- Code/build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ Code/build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.istudio.convention.ExtensionType
+import com.istudio.convention.configureBuildTypes
 import com.istudio.convention.configureKotlinAndroid
 import com.istudio.convention.libs
 import org.gradle.api.Plugin
@@ -23,6 +25,12 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 }
 
                 configureKotlinAndroid(this)
+
+                configureBuildTypes(
+                    commonExtension = this,
+                    extensionType = ExtensionType.APPLICATION,
+                    providers = providers
+                )
             }
         }
     }
---
+++ Code/build-logic/convention/src/main/java/com/istudio/convention/ExtensionType.kt
@@ -0,0 +1,6 @@
+package com.istudio.convention
+
+enum class ExtensionType {
+    APPLICATION,
+    LIBRARY
+}
