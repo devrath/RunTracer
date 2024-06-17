8a8ce179501403e5304c59a2ad230c2a535e12ce
Devrath <devrath.dev595@gmail.com>
2024-06-17 22:48:45 +0530

    added more files

--- Code/run/location/build.gradle.kts
+++ Code/run/location/build.gradle.kts
@@ -1,21 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    //alias(libs.plugins.runtracer.android.library)
+    alias(libs.plugins.runtracer.android.library)
 }
 
 android {
     namespace = "com.istudio.run.location"
-    compileSdk = 34
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
 
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.google.android.gms.play.services.location)
