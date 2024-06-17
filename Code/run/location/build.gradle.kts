a44ff2bbf3f986df4b6863cd2ad2deb8958cbb3b
Devrath <devrath.dev595@gmail.com>
2024-06-17 11:38:41 +0530

    added

--- Code/run/location/build.gradle.kts
+++ Code/run/location/build.gradle.kts
@@ -1,41 +1,15 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    //alias(libs.plugins.runtracer.android.library)
 }
 
 android {
     namespace = "com.istudio.run.location"
     compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    implementation(projects.core.domain)
-    implementation(projects.run.domain)
-
     implementation(libs.androidx.core.ktx)
 
     implementation(libs.androidx.ui)
@@ -45,4 +19,7 @@ dependencies {
 
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.google.android.gms.play.services.location)
+
+    implementation(projects.core.domain)
+    implementation(projects.run.domain)
 }
