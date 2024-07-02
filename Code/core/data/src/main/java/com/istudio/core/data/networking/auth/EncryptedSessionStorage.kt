3df47592d7d7cffaf32b22eefbb4508b7c0de727
Devrath <devrath.dev595@gmail.com>
2024-07-02 23:14:06 +0530

    added configurations

--- Code/core/data/src/main/java/com/istudio/core/data/networking/auth/EncryptedSessionStorage.kt
+++ Code/core/data/src/main/java/com/istudio/core/data/networking/auth/EncryptedSessionStorage.kt
@@ -1,5 +1,6 @@
 package com.istudio.core.data.networking.auth
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import com.istudio.core.domain.AuthInfo
 import com.istudio.core.domain.SessionStorage
@@ -21,13 +22,24 @@ class EncryptedSessionStorage(
         }
     }
 
+    /**
+     * Here it is used to set the auth info in the shared preferences
+     * (1) We should be able to pass the `NULL` value also so that if we do pass it, It will re-set the preferences.
+     *     It is useful when we want to clear the preferences when we log out
+     * (2)
+     *
+     * @param info
+     */
+    @SuppressLint("ApplySharedPref")
     override suspend fun set(info: AuthInfo?) {
         withContext(Dispatchers.IO) {
             if(info == null) {
-                sharedPreferences.edit().remove(KEY_AUTH_INFO).apply()
+                // We use commit since it is a blocking call
+                sharedPreferences.edit().remove(KEY_AUTH_INFO).commit()
                 return@withContext
             }
 
+            // If the info is not null, We have to save an actual auth info
             val json = Json.encodeToString(info.toAuthInfoSerializable())
             sharedPreferences
                 .edit()
--- Code/core/data/src/main/java/com/istudio/core/data/networking/di/CoreDataModule.kt
+++ Code/core/data/src/main/java/com/istudio/core/data/networking/di/CoreDataModule.kt
@@ -1,11 +1,15 @@
 package com.istudio.core.data.networking.di
 
 import com.istudio.core.data.networking.networking.HttpClientFactory
+import com.istudio.core.data.networking.auth.EncryptedSessionStorage
+import com.istudio.core.domain.SessionStorage
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
