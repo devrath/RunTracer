19707f407e5bf4d9674dffd87b668f47b414b794
Devrath <devrath.dev595@gmail.com>
2024-07-06 22:02:31 +0530

    added

--- Code/core/presentation/designsystem/src/screenshotTest/kotlin/Previews.kt
+++ Code/core/presentation/designsystem/src/screenshotTest/kotlin/Previews.kt
@@ -5,13 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.istudio.core.presentation.designsystem.RunTracerTheme
 import com.istudio.core.presentation.designsystem.components.RunTracerPasswordTextField
 import com.istudio.core.presentation.designsystem.preview.WindowSizeClassPreviews
 
 
-@Preview
+@WindowSizeClassPreviews
 @Composable
 private fun RunTracerTextFieldPreview() {
     RunTracerTheme {
