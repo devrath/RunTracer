31cb1071a2df12033aa128bd65fc1df10977ddb0
Devrath <devrath.dev595@gmail.com>
2024-07-25 19:36:09 +0530

    added new composables

--- Code/run/presentation/src/main/java/com/istudio/run/presentation/run_overview/RunOverviewScreen.kt
+++ Code/run/presentation/src/main/java/com/istudio/run/presentation/run_overview/RunOverviewScreen.kt
@@ -2,17 +2,24 @@
 
 package com.istudio.run.presentation.run_overview
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.istudio.core.presentation.designsystem.AnalyticsIcon
+import com.istudio.core.presentation.designsystem.LogoIcon
 import com.istudio.core.presentation.designsystem.LogoutIcon
 import com.istudio.core.presentation.designsystem.RunIcon
 import com.istudio.core.presentation.designsystem.RunTracerTheme
+import com.istudio.core.presentation.designsystem.components.RunTracerFloatingActionButton
+import com.istudio.core.presentation.designsystem.components.RunTracerScaffold
 import com.istudio.core.presentation.designsystem.components.RuniqueToolbar
 import com.istudio.core.presentation.designsystem.components.util.DropDownItem
 import com.istudio.run.presentation.R
@@ -35,7 +42,7 @@ private fun RunOverviewScreen(
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
         state = topAppBarState
     )
-    RuniqueScaffold(
+    RunTracerScaffold(
         topAppBar = {
             RuniqueToolbar(
                 showBackButton = false,
@@ -68,7 +75,7 @@ private fun RunOverviewScreen(
             )
         },
         floatingActionButton = {
-            RuniqueFloatingActionButton(
+            RunTracerFloatingActionButton(
                 icon = RunIcon,
                 onClick = {
                     onAction(RunOverviewAction.OnStartClick)
