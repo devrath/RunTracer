99abc963c6dbdad96eb512d4e37519d6ab4ded1d
Devrath <devrath.dev595@gmail.com>
2024-10-20 17:45:20 +0530

    fixed crash

--- Code/app/src/main/java/com/istudio/runtracer/RunTracerApplication.kt
+++ Code/app/src/main/java/com/istudio/runtracer/RunTracerApplication.kt
@@ -1,61 +1,42 @@
 package com.istudio.runtracer
 
 import android.app.Application
-import android.util.Log
+import androidx.work.WorkerFactory
 import com.istudio.auth.data.di.authDataModule
 import com.istudio.auth.presentation.di.authViewModelModule
 import com.istudio.core.data.networking.di.coreDataModule
 import com.istudio.run.location.di.locationModule
-import com.istudio.run.presentation.di.runViewModelModule
 import com.istudio.runtracer.di.appModule
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.workmanager.koin.workManagerFactory
-import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.component.KoinComponent
+import org.koin.core.context.startKoin
 import timber.log.Timber
-
 
 class RunTracerApplication : Application() {
 
-    companion object {
-        private val LOG_TAG = RunTracerApplication::class.simpleName
-
-    }
-
     val applicationScope = CoroutineScope(SupervisorJob())
-
 
     override fun onCreate() {
         super.onCreate()
-        // Logging
-        initTimber()
-        // Di
-        initKoin()
-    }
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
-    private fun initKoin() {
         startKoin {
             androidLogger()
             androidContext(this@RunTracerApplication)
             modules(
-                authViewModelModule,
                 authDataModule,
-                coreDataModule,
+                authViewModelModule,
                 appModule,
-                runViewModelModule,
+                coreDataModule,
                 locationModule
             )
         }
     }
-
-    private fun initTimber() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-
-
-
 }
